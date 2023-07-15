b0c49e5c117a94d06c8b575d0b3d4cfe3b07782e
htmlcssjvscrpt <131257637+htmlcssjvscrpt@users.noreply.github.com opai 5vjqSG5fLFh6ztL top E5TbFu5mDZjx6QW chekio 39xZQydmo>
2023-07-15 09:17:49 +0800

    Add tempConversion.js

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degrees) {
+    let conversion = (degrees - 32) * 5/9;
+    rounded = Math.round(conversion * 10) / 10;
+    return conversion;
+    // alert(rounded);
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degrees) {
+    let conversion = degrees * 9/5 + 32;
+    rounded = Math.round(conversion * 10) / 10;
+    return conversion;
+    // alert(rounded);
 };
+
+// convertToCelsius(-100);
+// convertToFahrenheit(-10);
+
 
 // Do not edit below this line
 module.exports = {
