90b2e0b34ea21e6c2d185b5f1019d45fc93792cf
htmlcssjvscrpt <131257637+htmlcssjvscrpt@users.noreply.github.com opai 5vjqSG5fLFh6ztL top E5TbFu5mDZjx6QW chekio 39xZQydmo>
2023-07-16 09:32:13 +0800

    Add calculator.js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(value1, value2) {
+  return value1 + value2;
 };
 
-const subtract = function() {
-	
+const subtract = function(value1, value2) {
+  return value1 - value2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0;
+  for (let value of array) {
+    sum += value;
+  }
+	return sum;
 };
 
 const multiply = function() {
-
+  let product = 1;
+  if (arguments.length !== 0) {
+    for (let i = 0; i < arguments.length; i++) {
+      product *= arguments[i];
+    }
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(value1, value2) {
+	return value1 ** value2;
 };
 
-const factorial = function() {
-	
+const factorial = function(value) {
+  let factorial = 1;
+  if (value === 0) {
+    factorial = 1;
+  } else {
+  for (let i = value; i > 0; i-- ) {
+    factorial *= i;
+  }
+  }
+  return factorial;
 };
 
 // Do not edit below this line
