d0ec3b823107b9b538e8f0d637e401a5c11fbd0b
htmlcssjvscrpt <131257637+htmlcssjvscrpt@users.noreply.github.com opai 5vjqSG5fLFh6ztL top E5TbFu5mDZjx6QW chekio 39xZQydmo>
2023-07-16 18:59:41 +0800

    Add palindromes.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,27 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let lowerCaseString = string.toLowerCase();
+    let stringToArray = lowerCaseString.split("");
+    let isPalindrome = true; //add a flag variable
 
-};
+    for (let i = 0; i < stringToArray.length; i++) {
+        if (!(/^[A-Za-z]$/.test(stringToArray[i]))) {
+            stringToArray.splice(i, 1);
+            i--;
+        }
+    }
 
+    for (let i = 0; i < stringToArray.length; i++) {
+        if (stringToArray[i] !== stringToArray[stringToArray.length - 1 -i]) {
+            isPalindrome = false; // set the flag to false if a non-matching pair is found
+            return false;
+            break;
+        } 
+    }
+
+    if (isPalindrome) { // Only alert "true" if the flag is still true after the loop
+        return true;
+    }
+}
+    
 // Do not edit below this line
 module.exports = palindromes;
