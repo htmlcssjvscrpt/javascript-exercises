277b190228c3d1cdcbfccfb0e9d70c599f440069
htmlcssjvscrpt <131257637+htmlcssjvscrpt@users.noreply.github.com opai 5vjqSG5fLFh6ztL top E5TbFu5mDZjx6QW chekio 39xZQydmo>
2023-07-17 09:16:41 +0800

    Add fibonacci.js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    let array = [1, 1, 2];
+    let num = number;
+    let fin_num = 0;
+    let sum = 0;
+    for (let i = 3; i === number; i ++) {
+        fin_num = array[number -2] + array[number -1 ];
+        array.push(fin_num);
+    }
+    alert(fin_num);
+    
 };
 
+fibonacci(4);
+
 // Do not edit below this line
-module.exports = fibonacci;
+// module.exports = fibonacci;
